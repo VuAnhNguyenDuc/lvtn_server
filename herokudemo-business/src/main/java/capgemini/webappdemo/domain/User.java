375d901744249ca8b58eca0d8eb0b217bece3875
Nguyễn Đức Vũ Anh <51300098@hcmut.edu.vn>
2017-10-09 22:26:13 +0700

    sad

--- herokudemo-business/src/main/java/capgemini/webappdemo/domain/User.java
+++ herokudemo-business/src/main/java/capgemini/webappdemo/domain/User.java
@@ -5,7 +5,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-@Entity(name = "user")
+@Entity(name = "app_user")
 public class User {
     @Id
     @GeneratedValue(strategy= GenerationType.AUTO)
