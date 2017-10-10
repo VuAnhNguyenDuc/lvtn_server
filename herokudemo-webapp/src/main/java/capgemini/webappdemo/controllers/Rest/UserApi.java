f5f0c8b66bb3668ce83bbf43a57d85ad8b1b75f1
Nguyen Duc Vu Anh <anh.ndv@sutrixsolutions.com>
2017-10-10 10:51:41 +0700

    sad

--- herokudemo-webapp/src/main/java/capgemini/webappdemo/controllers/Rest/UserApi.java
+++ herokudemo-webapp/src/main/java/capgemini/webappdemo/controllers/Rest/UserApi.java
@@ -12,20 +12,16 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
 
 /**
  * Created by Vu Anh Nguyen Duc on 10/10/2017.
  */
 
-@Controller
+@RestController
 public class UserApi {
     @Autowired
     private UserService userService;
