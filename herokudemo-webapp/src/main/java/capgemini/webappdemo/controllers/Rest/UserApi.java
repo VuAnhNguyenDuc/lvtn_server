93115b85a46283f59e8f4aef044c80e8c1cda702
Nguyễn Đức Vũ Anh <51300098@hcmut.edu.vn>
2017-10-10 08:21:43 +0700

    sad

--- herokudemo-webapp/src/main/java/capgemini/webappdemo/controllers/Rest/UserApi.java
+++ herokudemo-webapp/src/main/java/capgemini/webappdemo/controllers/Rest/UserApi.java
@@ -7,6 +7,7 @@ import capgemini.webappdemo.utils.JsonTokenUtil;
 import capgemini.webappdemo.utils.TokenPayload;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -31,7 +32,7 @@ public class UserApi {
         return new ResponseEntity<String>("Hello World", HttpStatus.OK);
     }
 
-    @RequestMapping(value = "/api/login", method = RequestMethod.POST)
+    @RequestMapping(value = "/api/login", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Message> loginApi(@ModelAttribute("user") User user, Errors errors, Model model){
         if(user.getUsername() != null && user.getPassword() != null){
             User result = userService.checkLogin(user.getUsername(),user.getPassword());
@@ -39,11 +40,11 @@ public class UserApi {
                 TokenPayload token = new TokenPayload(result.getId(),"manager");
                 String payload = jsonTokenUtil.createPayload(token);
                 String jsonKey = jsonTokenUtil.createJWT(payload);
-                return new ResponseEntity<>(new Message("Login successful", jsonKey), HttpStatus.OK);
+                return new ResponseEntity<Message>(new Message("Login successful", jsonKey), HttpStatus.OK);
             } else{
-                return new ResponseEntity<>(new Message("Incorrect username or password", ""), HttpStatus.OK);
+                return new ResponseEntity<Message>(new Message("Incorrect username or password", ""), HttpStatus.OK);
             }
         }
-        return new ResponseEntity<>(new Message("Username and Password cannot be empty!!", ""), HttpStatus.NO_CONTENT);
+        return new ResponseEntity<Message>(new Message("Username and Password cannot be empty!!", ""), HttpStatus.NO_CONTENT);
     }
 }
