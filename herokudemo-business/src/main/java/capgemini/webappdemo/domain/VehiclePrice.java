f9addb1f95f9bf0882225c7de65d1ff3cd58bf51
Nguyễn Đức Vũ Anh <51300098@hcmut.edu.vn>
2017-10-22 00:14:08 +0700

    working

--- herokudemo-business/src/main/java/capgemini/webappdemo/domain/VehiclePrice.java
+++ herokudemo-business/src/main/java/capgemini/webappdemo/domain/VehiclePrice.java
@@ -1,7 +1,6 @@
 package capgemini.webappdemo.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 /**
  * Created by Vu Anh Nguyen Duc on 10/21/2017.
@@ -9,8 +8,19 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "vehicle_price")
 public class VehiclePrice {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private Integer id;
     private String key;
     private double value;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
 
     public String getKey() {
         return key;
