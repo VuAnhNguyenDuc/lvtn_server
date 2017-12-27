1d16dda8001cecb9a37c71a42f4fff8a469f709d
Nguyễn Đức Vũ Anh <51300098@hcmut.edu.vn>
2017-12-27 23:29:40 +0700

    working

--- herokudemo-webapp/src/main/java/capgemini/webappdemo/controllers/Web/PopulateTestData.java
+++ herokudemo-webapp/src/main/java/capgemini/webappdemo/controllers/Web/PopulateTestData.java
@@ -3,10 +3,12 @@ package capgemini.webappdemo.controllers.Web;
 import capgemini.webappdemo.domain.*;
 import capgemini.webappdemo.service.Admin.AdminService;
 import capgemini.webappdemo.service.Appointment.AppointmentService;
+import capgemini.webappdemo.service.Client.ClientService;
 import capgemini.webappdemo.service.Coordinate.CoordinateService;
 import capgemini.webappdemo.service.Detail.DetailService;
 import capgemini.webappdemo.service.Employee.EmployeeService;
 import capgemini.webappdemo.service.Manager.ManagerService;
+import capgemini.webappdemo.service.SpecialPlace.SpecialPlaceService;
 import capgemini.webappdemo.service.User.UserService;
 import capgemini.webappdemo.service.Vehicle.VehicleService;
 import capgemini.webappdemo.utils.CalculateMoney;
@@ -52,16 +54,18 @@ public class PopulateTestData {
     @Autowired
     private DetailService dtService;
 
+    @Autowired
+    private ClientService clService;
+
+    @Autowired
+    private SpecialPlaceService spService;
+
     private DateFormat dateFormat = new SimpleDateFormat("HH:mm:ss dd-MM-yyyy");
     private CommonUtils cu = new CommonUtils();
 
-    @RequestMapping(value = "/populateData", method = RequestMethod.GET)
-    public void populateData() throws ParseException {
-        /*CalculateMoney cm = new CalculateMoney();
-        double cost = cm.test("{\"id\":16,\"formulas\":[{\"condition_type\":\"if\",\"condition\":\"1 == 0\",\"formula\":\"1 + 1\"},{\"condition_type\":\"else\",\"condition\":\"\",\"formula\":\"5 + 5\"}],\"vars\":[]}",10.0,60);
-        System.out.println("cost = " + cost);*/
-
-        /*Admin admin = new Admin();
+    @RequestMapping(value = "/createUsers", method = RequestMethod.GET)
+    public void createUsers(){
+        Admin admin = new Admin();
         admin.setUsername("admin");
         admin.setPassword("admin");
         adminService.add(admin);
@@ -69,8 +73,8 @@ public class PopulateTestData {
         User mng = new User();
         mng.setUsername("mng");
         mng.setPassword("mng");
-        mng.setFullname("Vu Anh");
-        mng.setEmail("mng@mgial.com");
+        mng.setFullname("Nguyễn Đức Vũ Anh");
+        mng.setEmail("mng@gmail.com");
         mng.setStatus(1);
         userService.add(mng);
 
@@ -80,20 +84,6 @@ public class PopulateTestData {
         mngM.setNumber_of_employees(0);
         managerService.add(mngM);
 
-        User emp = new User();
-        emp.setUsername("emp");
-        emp.setPassword("emp");
-        emp.setFullname("Test Employee");
-        emp.setEmail("emp@gmail.com");
-        emp.setStatus(1);
-        userService.add(emp);
-
-        Employee empE = new Employee();
-        empE.setUser_id(emp.getId());
-        empE.setStatus(1);
-        empE.setManager_id(mng.getId());
-        empE.setEmployee_type("Employee");
-        employeeService.add(empE);
 
         User dung = new User();
         dung.setUsername("dung");
@@ -103,12 +93,12 @@ public class PopulateTestData {
         dung.setStatus(1);
         userService.add(dung);
 
-        empE = new Employee();
-        empE.setUser_id(dung.getId());
-        empE.setStatus(1);
-        empE.setManager_id(mng.getId());
-        empE.setEmployee_type("Employee");
-        employeeService.add(empE);
+        Employee emp = new Employee();
+        emp.setUser_id(dung.getId());
+        emp.setStatus(1);
+        emp.setManager_id(mng.getId());
+        emp.setEmployee_type("Employee");
+        employeeService.add(emp);
 
         User dam = new User();
         dam.setUsername("dam");
@@ -118,14 +108,103 @@ public class PopulateTestData {
         dam.setStatus(1);
         userService.add(dam);
 
-        empE = new Employee();
-        empE.setUser_id(dam.getId());
-        empE.setStatus(1);
-        empE.setManager_id(mng.getId());
-        empE.setEmployee_type("Employee");
-        employeeService.add(empE);*/
+        emp = new Employee();
+        emp.setUser_id(dam.getId());
+        emp.setStatus(1);
+        emp.setManager_id(mng.getId());
+        emp.setEmployee_type("Employee");
+        employeeService.add(emp);
+    }
+
+    @RequestMapping(value = "/createClient", method = RequestMethod.GET)
+    public void createClient(){
+        Client cl = new Client();
+        cl.setName("Vũ Hoàng Hương");
+        cl.setEmail("vuong@gmail.com");
+        cl.setPhone_number("0976765446");
+        cl.setAddress("Biên Hòa, Đồng Nai");
+        clService.add(cl);
+    }
+
+    @RequestMapping(value = "/createVehicles", method = RequestMethod.GET)
+    public void createVehicles(){
+        Vehicle v1 = new Vehicle("Uber X");
+        v1.setCalculatable(true);
+        v1.setStatus(1);
+        Vehicle v2 = new Vehicle("Uber Black");
+        v2.setCalculatable(true);
+        v2.setStatus(1);
+        Vehicle v3 = new Vehicle("Uber SUV");
+        v3.setCalculatable(true);
+        v3.setStatus(1);
+        Vehicle v4 = new Vehicle("Uber MOTO");
+        v4.setCalculatable(true);
+        v4.setStatus(1);
+        Vehicle v5 = new Vehicle("Grab Bike");
+        v5.setCalculatable(true);
+        v5.setStatus(1);
+        Vehicle v6 = new Vehicle("Grab Bike Premium");
+        v6.setCalculatable(true);
+        v6.setStatus(1);
+        Vehicle v7 = new Vehicle("Grab Car 4 seats");
+        v7.setCalculatable(true);
+        v7.setStatus(1);
+        Vehicle v8 = new Vehicle("Grab Car 7 seats");
+        v8.setCalculatable(true);
+        v8.setStatus(1);
+        Vehicle v9 = new Vehicle("Air plane");
+        v9.setCalculatable(true);
+        v9.setStatus(1);
+        Vehicle v10 = new Vehicle("Train");
+        v10.setCalculatable(true);
+        v10.setStatus(1);
+        Vehicle v11 = new Vehicle("Boat");
+        v11.setCalculatable(true);
+        v11.setStatus(1);
+        vhcService.add(v1);
+        vhcService.add(v2);
+        vhcService.add(v3);
+        vhcService.add(v4);
+        vhcService.add(v5);
+        vhcService.add(v6);
+        vhcService.add(v7);
+        vhcService.add(v8);
+        vhcService.add(v9);
+        vhcService.add(v10);
+        vhcService.add(v11);
+    }
+
+    @RequestMapping(value = "/createSpecialPlaces",method = RequestMethod.GET)
+    public void createSpecialPlaces(){
+        SpecialPlace sp = new SpecialPlace();
+        sp.setName("Cat Lai Harbor");
+        sp.setType("Harbor");
+        sp.setRange(2000);
+        sp.setLatitude(10.755063);
+        sp.setLongitude(106.7883273);
+        sp.setStatus(1);
+        spService.add(sp);
+
+        sp.setName("Tan Son Nhat Airport");
+        sp.setType("Airport");
+        sp.setRange(1000);
+        sp.setLatitude(10.8184631);
+        sp.setLongitude(106.6566358);
+        sp.setStatus(1);
+        spService.add(sp);
+
+        sp.setName("Saigon Train Station");
+        sp.setType("Train Station");
+        sp.setRange(500);
+        sp.setLatitude(10.7823643);
+        sp.setLongitude(106.6749899);
+        sp.setStatus(1);
+        spService.add(sp);
+    }
 
 
+    @RequestMapping(value = "/populateData", method = RequestMethod.GET)
+    public void populateData() throws ParseException {
         /*Appointment apm = new Appointment();
         apm.setName("Appointment Demo");
         apm.setManager_id(28);
@@ -240,39 +319,40 @@ public class PopulateTestData {
 
         /*vhcService.deleteAll();
 
-        Vehicle v1 = new Vehicle("Uber X");
-        v1.setCalculatable(true);
-        v1.setStatus(1);
-        Vehicle v2 = new Vehicle("Uber Black");
-        v2.setCalculatable(true);
-        v2.setStatus(1);
-        Vehicle v3 = new Vehicle("Uber SUV");
-        v3.setCalculatable(true);
-        v3.setStatus(1);
-        Vehicle v4 = new Vehicle("Uber MOTO");
-        v4.setCalculatable(true);
-        v4.setStatus(1);
-        Vehicle v5 = new Vehicle("Grab Bike");
-        v5.setCalculatable(true);
-        v5.setStatus(1);
-        Vehicle v6 = new Vehicle("Grab Bike Premium");
-        v6.setCalculatable(true);
-        v6.setStatus(1);
-        Vehicle v7 = new Vehicle("Grab Car 4 seats");
-        v7.setCalculatable(true);
-        v7.setStatus(1);
-        Vehicle v8 = new Vehicle("Grab Car 7 seats");
-        v8.setCalculatable(true);
-        v8.setStatus(1);
-        vhcService.add(v1);
-        vhcService.add(v2);
-        vhcService.add(v3);
-        vhcService.add(v4);
-        vhcService.add(v5);
-        vhcService.add(v6);
-        vhcService.add(v7);
-        vhcService.add(v8);*/
+        */
 
         coorService.deleteAll();
     }
+
+    /*
+    formulas
+    Grab Car 7 seats
+    if s<=2  24 + 0.3*t/60
+    else 24 + 11*s + 0.3*t/60
+
+    Grab Car 4 seats
+    if s<=2 20 + 0.3*t/60
+    else 20 + 9*s + 0.3*t/60
+
+    Grab Bike Premium
+    if s<=2 20
+    else 20 + 7*(s-2)
+
+    Grab Bike
+    if s<=2 12
+    else 12 + 3.8*(s-2)
+
+    UberMoto
+    10 + 3.7*s + 0.2*t/60
+
+    Uber SUV
+    5 + 9.597*s + 0.8*t/60
+
+    Uber Black
+    5 + 9.597*s + 0.8*t/60
+
+    Uber X
+    15 + 7.5*s + 0.3*t/60
+
+    */
 }
